d37dc96803359d7f50a2ff02df4840a7dd86552d
li-qiang <kata.Qlee@gmail.com>
2016-07-31 10:48:22 +0800

    Fix when type is diffrent

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 export default (target, fn = i => i) => {
-	let map = {};
-	return target.filter((item) => {
-		let key = fn(item);
-		if (map[key]) return false;
-		map[key] = item
-		return true;
-	});
+  let map = {};
+
+  return target.filter((item) => {
+    let key = fn(item);
+    let type = typeof(key);
+    map[type] = map[type] || {};
+    if (map[type].hasOwnProperty(key)) return false;
+    map[type][key] = item;
+    return true;
+  });
 }
--- test/index-spec.js
+++ test/index-spec.js
@@ -3,10 +3,41 @@ import uniq from '../index';
 import {expect} from 'chai';
 
 describe('Uniq', () => {
-	it('should remove repeatable item', () => {
-		let a = [1, 2, 3, 2];
 
-		expect(uniq(a)).to.deep.equal([1, 2, 3]);
+	it('should remove repeatable item', () => {
+		let a = [0, 1, 2, 3, 0, 'undefined', 2, undefined, false, false, undefined];
+
+		expect(uniq(a)).to.deep.equal([0, 1, 2, 3, 'undefined', undefined, false]);
+	});
+
+	it(`should remove repeatable item when undefined`, () => {
+		let res = uniq([undefined, undefined, undefined, 'undefined']);
+		expect(res).to.deep.equal([undefined, 'undefined']);
+	});
+
+	it(`should remove repeatable item when null`, () => {
+		let res = uniq([null, null, null, 'null']);
+		expect(res).to.deep.equal([null, 'null']);
+	});
+
+	it(`should remove repeatable item when false`, () => {
+		let res = uniq([false, false, false, 'false']);
+		expect(res).to.deep.equal([false, 'false']);
+	});
+
+	it(`should remove repeatable item when true`, () => {
+		let res = uniq([true, true, true, 'true']);
+		expect(res).to.deep.equal([true, 'true']);
+	});
+
+	it(`should remove repeatable item when 0`, () => {
+		let res = uniq([0, 0, 0, '0']);
+		expect(res).to.deep.equal([0, '0']);
+	});
+
+	it('should remove repeatable item when ', () => {
+		let a = [0, 1, 2, 3, 0];
+		expect(uniq(a)).to.deep.equal([0, 1, 2, 3]);
 	});
 
 	it('should remove repeatable item', () => {
